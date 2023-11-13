2e7a3e6f459b73d0eac12d6ac6d8004f12fc9cc6
alondra zurita <alondrazurita2@gmail.com>
2023-11-13 00:15:53 -0600

    added into the database file

--- main/client/src/js/database.js
+++ main/client/src/js/database.js
@@ -13,9 +13,57 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+try {
+  const db = await openDB('jete', 1);
+if (!db.objectStoreNames.contains('jate')) {
+console.error('putDb not implemented');
+return;
+}
 
+const tx = db.transaction('jate', 'readwrite'); 
+const store = tx.objectStore('jate'); 
+const request = store.add({ content }); 
+
+await tx.done;
+
+request.onsuccess = () => {
+  console.log('Content added to the database successfully');
+};
+
+request.onerror = (event) => {
+  console.error('Error adding content to the database:', event.target.error);
+};
+} catch (error) {
+console.error('Error:', error);
+}
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1); 
+
+    if (!db.objectStoreNames.contains('jate')) {
+      console.error('jate object store does not exist');
+      return [];
+    }
+
+    const tx = db.transaction('jate', 'readonly'); 
+    const store = tx.objectStore('jate'); 
+    const request = store.getAll(); 
+    await tx.done;
+
+    request.onsuccess = (event) => {
+      const allData = event.target.result;
+      console.log('All content retrieved from the database:', allData);
+    };
+
+    request.onerror = (event) => {
+      console.error('Error retrieving content from the database:', event.target.error);
+    };
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
 initdb();
